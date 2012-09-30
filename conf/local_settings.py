fe2228f0b7fd484509612e53126d57e067cf5f04
Markos Gogoulos <mgogoulos@unweb.me>
2012-09-30 21:47:55 +0300

    add graphite on buildout

---
+++ conf/local_settings.py
@@ -0,0 +1,14 @@
+## Graphite local_settings.py
+# Edit this file to customize the default Graphite webapp settings
+
+DATABASES = {
+    'default': {
+        'NAME': 'graphite.db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
+    }
+}
+
