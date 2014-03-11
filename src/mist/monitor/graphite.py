0cc6c37c5bca017c2838205f64f0294796979364
Dimitris Rozakis <dimrozakis@mist.io>
2014-03-11 19:43:39 +0200

    Update graphite to optionally use bucky's targets

--- src/mist/monitor/graphite.py
+++ src/mist/monitor/graphite.py
@@ -25,13 +25,15 @@ class BaseGraphiteSeries(object):
         """A uuid is required to initialize the class."""
         self.uuid = uuid
 
-    @property
-    def head(self):
+    def head(self, bucky=False):
         """Top level data target."""
-        return "mist-%s" % (self.uuid)
+        if not bucky:
+            return "mist-%s" % self.uuid
+        else:
+            return "bucky.%s" % self.uuid
 
     @abc.abstractmethod
-    def get_targets(self, interval_str=''):
+    def get_targets(self, interval_str='', bucky=False):
         """Return list of target strings.
 
         If interval_str specified, summarize targets accordingly.
@@ -39,7 +41,8 @@ class BaseGraphiteSeries(object):
         """
         return []
 
-    def get_series(self, start="", stop="", interval_str="", transform_null=None):
+    def get_series(self, start="", stop="", interval_str="",
+                   transform_null=None, bucky=False):
         """Get time series from graphite.
 
         Optional start and stop parameters define time range.
@@ -73,7 +76,7 @@ class BaseGraphiteSeries(object):
         else:
             filter_from = 0
 
-        targets = self.get_targets(interval_str=interval_str)
+        targets = self.get_targets(interval_str=interval_str, bucky=bucky)
         uri = self._construct_graphite_uri(targets, start, stop)
         data = self._graphite_request(uri, filter_from=filter_from)
         return self._post_process_series(data, transform_null=transform_null)
@@ -172,7 +175,7 @@ class BaseGraphiteSeries(object):
                 # If we try to get another metric, say Load, on such a target,
                 # we will get a 200 OK response but the asked target will be
                 # missing from the response body.
-                # TODO: send a query to "metrics?query=%s" % self.head
+                # TODO: send a query to "metrics?query=%s" % self.head()
                 # to reveal if graphite knows the machine or not.
                 reason = ("Trying to do division in an invalid target, maybe "
                           "the machine never sent Graphite any data.")
@@ -218,8 +221,15 @@ class SimpleSingleGraphiteSeries(SingleGraphiteSeries):
     def get_inner_target(self):
         pass
 
-    def get_targets(self, interval_str=""):
-        target = self.get_inner_target()
+    @abc.abstractmethod
+    def get_inner_target_bucky(self):
+        pass
+
+    def get_targets(self, interval_str="", bucky=False):
+        if bucky:
+            target = self.get_inner_target_bucky()
+        else:
+            target = self.get_inner_target()
         if interval_str:
             target = "summarize(%s,'%s','%s')" % (target, interval_str,
                                                   self.sum_function)
@@ -247,11 +257,12 @@ class CombinedGraphiteSeries(BaseGraphiteSeries):
                                 "BaseGraphiteSeries." % series)
         self.series_list = series_list
 
-    def get_targets(self, interval_str=""):
+    def get_targets(self, interval_str="", bucky=False):
         targets = []
         # join all child series targets
         for series in self.series_list:
-            targets += series.get_targets(interval_str=interval_str)
+            targets += series.get_targets(interval_str=interval_str,
+                                          bucky=bucky)
         # remove duplicates (using a dict since lookup is a lot faster)
         seen = {}
         uniq_targets = []
@@ -277,16 +288,23 @@ class CpuUtilSeries(SimpleSingleGraphiteSeries):
 
     def get_inner_target(self):
         # Calculate the sum of all time measurements, excluding the "idle" one
-        total_wo_idle_sum = "sumSeries(exclude(%s.cpu-0.*,'idle'))" % (self.head)
+        total_wo_idle_sum = "sumSeries(exclude(%s.cpu-0.*,'idle'))" % self.head()
         # Calculate the sum of all time measurements
-        total_sum = "sumSeries(%s.cpu-0.*)" % (self.head)
+        total_sum = "sumSeries(%s.cpu-0.*)" % self.head()
         # Calculate the derivative of each sum
-        first_set = "nonNegativeDerivative(%s)" % (total_wo_idle_sum)
-        second_set = "nonNegativeDerivative(%s)" % (total_sum)
+        first_set = "nonNegativeDerivative(%s)" % total_wo_idle_sum
+        second_set = "nonNegativeDerivative(%s)" % total_sum
         # Divide the first with the second sum (wo_idle_sum / total_sum)
         #target = "divideSeries(%s,%s)" % (first_set, second_set)
         target = "asPercent(%s,%s)" % (first_set, second_set)
         return target
+
+    def get_inner_target_bucky(self):
+        head = self.head(bucky=True)
+        non_idle = "sumSeries(exclude(%s.cpu.*.*,'idle'))" % head
+        total = "sumSeries(%s.cpu.*.*)" % head
+        perc = "asPercent(%s,%s)" % (non_idle, total)
+        return perc
 
 
 class CpuAllSeries(CombinedGraphiteSeries):
@@ -312,7 +330,10 @@ class LoadSeries(SimpleSingleGraphiteSeries):
     sum_function = "avg"
 
     def get_inner_target(self):
-        return "%s.load.load.shortterm" % (self.head)
+        return "%s.load.load.shortterm" % (self.head())
+
+    def get_inner_target_bucky(self):
+        return "%s.load.shortterm" % self.head(bucky=True)
 
 
 class NetSeries(SimpleSingleGraphiteSeries):
@@ -334,10 +355,15 @@ class NetSeries(SimpleSingleGraphiteSeries):
         # collectd version 5.1  uses 'interface-eth0.if_octets' format
         # we use the {-eth0,} filter in both places to catch both cases
         raw_series = "%s.interface{-%s,}.if_octets{-%s,}.%s" % \
-            (self.head, self.iface, self.iface, self.direction)
+            (self.head(), self.iface, self.iface, self.direction)
         net_util = "scaleToSeconds(sumSeries(nonNegativeDerivative(%s)),1)" % \
             (raw_series, )
         return net_util
+
+    def get_inner_target_bucky(self):
+        return "%s.interface.%s.if_octets.%s" % (
+            self.head(bucky=True), self.iface, self.direction
+        )
 
 
 class NetRxSeries(NetSeries):
@@ -378,10 +404,17 @@ class MemSeries(SimpleSingleGraphiteSeries):
     sum_function = "avg"
 
     def get_inner_target(self):
-        target_used = 'sumSeries(%s.memory.memory-{buffered,cached,used})' % (self.head)
-        target_total= 'sumSeries(%s.memory.memory-*)' % (self.head)
+        target_used = 'sumSeries(%s.memory.memory-{buffered,cached,used})' % (self.head())
+        target_total= 'sumSeries(%s.memory.memory-*)' % (self.head())
         target_perc = 'asPercent(%s, %s)' % (target_used, target_total)
         return target_perc
+
+    def get_inner_target_bucky(self):
+        head = self.head(bucky=True)
+        non_free = "sumSeries(%s.memory.{buffered,cached,used})" % head
+        total = "sumSeries(%s.memory.*)" % head
+        perc = "asPercent(%s,%s)" % (non_free, total)
+        return perc
 
 
 class DiskSeries(SimpleSingleGraphiteSeries):
@@ -389,15 +422,23 @@ class DiskSeries(SimpleSingleGraphiteSeries):
     alias = "disk"
     sum_function = "avg"
     direction = "*"
+    disk = "*"
 
-    def __init__(self, uuid, alias="", direction=""):
+    def __init__(self, uuid, alias="", direction="", disk=""):
         if direction:
             self.direction = direction
+        if disk:
+            self.disk = disk
         super(DiskSeries, self).__init__(uuid, alias=alias)
 
     def get_inner_target(self):
         return "scaleToSeconds(nonNegativeDerivative(sumSeries(%s.disk-*.%s.%s)),1)" % (
-            self.head, 'disk_octets', self.direction
+            self.head(), 'disk_octets', self.direction
+        )
+
+    def get_inner_target_bucky(self):
+        return "sumSeries(%s.disk.%s.disk_octets.%s)" % (
+            self.head(bucky=True), self.disk, self.direction
         )
 
 
