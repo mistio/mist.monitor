3134904772983263d78dacece3cc6350b3f4662c
Dimitris Rozakis <dimrozakis@mist.io>
2014-06-16 20:46:34 +0300

    remove wrong arg from ping converter init

--- src/mist/bucky_extras/collectd_converters/ping.py
+++ src/mist/bucky_extras/collectd_converters/ping.py
@@ -2,7 +2,7 @@ from bucky.collectd import DefaultConverter
 
 class PingConverter(object):
     PRIORITY = 0
-    def __init__(self, sample):
+    def __init__(self):
         self.default_converter = DefaultConverter()
     def __call__(self, sample):
         print sample
