c1d7bc0bf3a17c9eb662fb7c39a4d035a5ad19e0
Dimitris Rozakis <dimrozakis@mist.io>
2014-06-16 20:39:22 +0300

    Attempt to fix pinged ip issue in graphite with custom collectd converter in bucky

---
+++ src/mist/bucky_extras/collectd_converters/ping.py
@@ -0,0 +1,9 @@
+from bucky.collectd import DefaultConverter
+
+class PingConverter(object):
+    PRIORITY = 0
+    def __init__(self, sample):
+        self.default_converter = DefaultConverter()
+    def __call__(self, sample):
+        print sample
+        return self.default_converter(sample)
