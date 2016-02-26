12bcc3d0e297f470e78a4fc64e0d9c4c7677ef56
Dimitris Moraitis <dimo@mist.io>
2016-02-26 13:11:45 +0200

    remove unused code

--- src/mist/monitor/config.py
+++ src/mist/monitor/config.py
@@ -1,21 +1,8 @@
 """Parses user defined settings from settings.py in top level dir."""
-import socket
-import fcntl
-import struct
-
 import logging
 import os
 
 log = logging.getLogger(__name__)
-
-
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
 
 
 # Parse user defined settings from settings.py in the top level project dir
