fffc0184536fa46835dd8e674c31faba6f123ba5
commixon <commixon@gmail.com>
2018-01-20 16:09:07 +0200

    Update graphite container

--- containers/graphite/graphite-web.sh
+++ containers/graphite/graphite-web.sh
@@ -2,14 +2,15 @@
 
 # sh /opt/graphite/bin/build-index.sh
 
+
 uwsgi --plugin /usr/lib/uwsgi/python_plugin.so \
-      --uid root \
-      --master \
-      --master-as-root \
-      --processes 4 \
-      --die-on-term \
-      --http-socket 0.0.0.0:80 \
-      --module 'django.core.handlers.wsgi:WSGIHandler()' \
-      --chdir /opt/graphite/webapp \
-      --harakiri 120 \
-      --max-requests 50
+    --uid root \
+    --master \
+    --master-as-root \
+    --processes 4 \
+    --die-on-term \
+    --http-socket 0.0.0.0:8000 \
+    --wsgi-file /opt/graphite/webapp/graphite/wsgi.py \
+    --chdir /opt/graphite/webapp \
+    --harakiri 120 \
+    --max-requests 50
--- containers/graphite/local_settings.py
+++ containers/graphite/local_settings.py
@@ -3,6 +3,23 @@ CLUSTER_SERVERS = []
 # List all local running carbon-cache instances (their cache query ports)
 CARBONLINK_HOSTS = ['127.0.0.1:7002']
 
+DASHBOARD_REQUIRE_AUTHENTICATION = False
 LOG_RENDERING_PERFORMANCE = False
 LOG_CACHE_PERFORMANCE = False
 LOG_METRIC_ACCESS = False
+
+STATIC_ROOT = '/opt/graphite/webapp/content/'
+STATIC_URL = '/content/'
+
+STATICFILES_DIRS = (
+
+#    os.path.join(BASE_DIR, "static"),
+#    os.path.join(BASE_DIR, "content"),
+    "/opt/graphite/webapp/content/",
+
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
